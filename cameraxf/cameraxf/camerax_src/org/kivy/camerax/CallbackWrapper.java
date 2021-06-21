1dc016b545d88e66802469be576d9e91bb12f3d5
RobertF <34464649+RobertFlatt@users.noreply.github.com>
2021-06-20 14:14:43 -1000

    Delete CallbackWrapper.java

--- cameraxf/cameraxf/camerax_src/org/kivy/camerax/CallbackWrapper.java
+++
@@ -1,9 +0,0 @@
-package org.kivy.camerax;
-
-import androidx.camera.core.ImageProxy;
-
-public interface CallbackWrapper {
-    public void callback_image(ImageProxy image);
-    public void callback_string(String filepath);
-}
-
